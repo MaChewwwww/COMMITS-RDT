8b2144bfe97aaed37898049e66f93e93e04bd2a4
Zanti <mobaraqcamar@gmail.com>
2024-12-13 19:46:11 +0800

    Added comments on web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ReportController;
 use Illuminate\Support\Facades\Route;
 
+# Will make the index the default page
 Route::get('/', [ReportController::class, 'index']);
 
 Route::resource('report', ReportController::class);
