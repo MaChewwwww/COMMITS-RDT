34700f2d07177b0319ab5610fb2bcad2942318ee
villacarlos <villacarlosmichelle28@gmail.com>
2024-12-17 03:48:24 +0800

    Add filters and update UI for HISTORY views (blade files)

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,28 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('HISTORY.all');
 });
+
+Route::get('/all', function () {
+    return view('HISTORY.all');
+});
+
+Route::get('/student', function () {
+    return view('HISTORY.student');
+});
+
+Route::get('/faculty', function () {
+    return view('HISTORY.faculty');
+});
+
+Route::get('/visitor', function () {
+    return view('HISTORY.visitor');
+});
+
+Route::get('/dependent', function () {
+    return view('HISTORY.dependent');
+});
+
+
+
