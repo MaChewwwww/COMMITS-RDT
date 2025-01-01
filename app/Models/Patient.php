2bb9a80a33d0bafd5208f319ae7cefe3a87fd9b5
John Christian Linaban <39191027+JChristian25@users.noreply.github.com>
2025-01-01 10:10:21 +0800

    hapi new yir

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -2,11 +2,15 @@
 
 namespace App\Models;
 
+<<<<<<< Updated upstream
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+=======
+>>>>>>> Stashed changes
 use Illuminate\Database\Eloquent\Model;
 
 class Patient extends Model
 {
+<<<<<<< Updated upstream
     use HasFactory;
 
     protected $fillable = [
@@ -22,5 +26,17 @@ class Patient extends Model
         'registrationDate',
         'isFullyHealed',
         'assignedDoctorPhysician'
+=======
+    //
+
+    protected $fillable = [
+        'fullname',
+        'sex',
+        'year_course_dept',
+        'contactDetails',
+        'patient_status',
+        'patientType',
+        'user_id',
+>>>>>>> Stashed changes
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,27 @@ Route::get('/', function () {
 });
 
 Route::prefix('patient')->group(function () {
+<<<<<<< Updated upstream
 
     Route::get('/', [PatientController::class, "index"])->name('patients');
 
+=======
+    // Show all patients (index page)
+    Route::get('/', [PatientController::class, 'index'])->name('patients');
+    
+    // Show form to add a new patient
+    Route::get('/new', [PatientController::class, 'add'])->name('patients.add');
+    
+    // Store a new patient
+    Route::post('/store', [PatientController::class, 'store'])->name('patients.store');
+    
+    // Show the edit form for a specific patient
+    Route::get('/edit/{id}', [PatientController::class, 'edit'])->name('patients.edit');
+    
+    // Update a patient record
+    Route::put('/update/{id}', [PatientController::class, 'update'])->name('patients.update');
+    
+    // Delete a patient record
+    Route::delete('/destroy/{id}', [PatientController::class, 'destroy'])->name('patients.destroy');
+>>>>>>> Stashed changes
 });
